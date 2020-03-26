b2a7c616782fd50ac18782ce12a81f61ab1d3c62
Justin Lycklama <lycklama.justin@gmail.com>
2020-03-26 12:50:55 -0400

    Basic touch controls for pan + pinch zoom

--- Assets/Game Management/PlayerBehaviour.cs
+++ Assets/Game Management/PlayerBehaviour.cs
@@ -35,12 +35,116 @@ public class PlayerBehaviour : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+
+        MobileUIUpdates();
+
         CheckButtonInput();
 
         Vector2 mousePos = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
         if(!UIRect.Contains(mousePos)) {
             SelectGameObject();
         }
+    }
+
+    Vector2? previousTouch = null;
+
+    Vector2? residualDirection = null;
+    float friction = 1.0f;
+
+
+    public float zoomOutMin = 1;
+    public float zoomOutMax = 8;
+
+    void MobileUIUpdates() {
+
+        var cameraForward = Camera.main.transform.forward;
+        cameraForward.y = 0f;
+        cameraForward.Normalize();
+
+        var cameraRight = Camera.main.transform.right;
+        cameraForward.y = 0f;
+        cameraForward.Normalize();
+
+        if(Input.GetMouseButton(0) && Input.touchCount < 2) {
+            Vector2 touch = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+            if (previousTouch.HasValue) {
+                Vector2 direction = (touch - previousTouch.Value) * 0.05f;
+
+                print("Drection " + direction);
+
+                cameraRight.x *= direction.x;
+                cameraForward.z *= direction.y;
+
+                var finalDirection = cameraForward + cameraRight;
+
+                Camera.main.transform.Translate(-finalDirection * cameraMovementSpeed * Time.deltaTime, Space.World);
+                residualDirection = direction;
+            }
+
+            previousTouch = touch;
+        } else {
+            previousTouch = null;
+
+            if (residualDirection.HasValue) {
+
+                cameraRight.x *= residualDirection.Value.x;
+                cameraForward.z *= residualDirection.Value.y;
+
+                var finalDirection = cameraForward + cameraRight;
+
+                Camera.main.transform.Translate(-finalDirection * cameraMovementSpeed * Time.deltaTime, Space.World);
+
+                residualDirection -= residualDirection * 0.05f;
+
+                if(Mathf.Abs(residualDirection.Value.x) < 0.01 && Mathf.Abs(residualDirection.Value.y) < 0.01) {
+                    residualDirection = null;
+                }
+            }
+        }
+
+
+        //if(Input.GetMouseButtonDown(0)) {
+        //    touchStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //    print("Mouse Down");
+        //}
+
+        // Mouse Zoom
+        zoom(Input.GetAxis("Mouse ScrollWheel") * 100f);
+
+        // Finger Pinch Zoom
+        if(Input.touchCount == 2) {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevMagnitude = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float currentMagnitude = (touchZero.position - touchOne.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+
+            zoom(difference * 0.01f);
+        } 
+        
+        //else if(Input.GetMouseButton(0)) {
+        //    Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //    Camera.main.transform.position += direction;
+        //    print("Mouse Dragging");
+        //}
+
+        
+    }
+
+    void zoom(float increment) {
+
+        var cameraUp = Camera.main.transform.forward; 
+        cameraUp.Normalize();
+
+        cameraUp *= increment;
+
+        Camera.main.transform.Translate(cameraUp * cameraMovementSpeed * Time.deltaTime, Space.World);
     }
 
     void CheckButtonInput() {
@@ -50,8 +154,8 @@ public class PlayerBehaviour : MonoBehaviour {
         cameraForward.Normalize();
 
         var cameraRight = Camera.main.transform.right;
-        cameraForward.y = 0f;
-        cameraForward.Normalize();
+        cameraRight.y = 0f;
+        cameraRight.Normalize();
 
         // Camera Move
         if(Input.GetKey(KeyCode.D)) {
