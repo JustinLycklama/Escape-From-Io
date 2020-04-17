8a2e2d140d6982854ec743c5ffad0d845a2e8049
Justin Lycklama <lycklama.justin@gmail.com>
2020-04-17 10:00:45 -0400

    Adding new files for elemental and test texture and icons

---
+++ Assets/Game Objects/L3D_Assets/EarthElementalController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthElementalController : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start() {
+        //animator.speed = 1.5f;
+    }
+
+    public void Idle() {
+        animator.speed = 1.0f;
+        animator.Play("idle");
+    }
+
+    public void IdleActivate() {
+        animator.Play("idleActivate");
+    }
+
+    public void Walk() {
+        animator.speed = 1.5f;
+        animator.Play("walk");
+    }
+
+    public void Run() {
+        animator.Play("run");
+    }
+
+    public void Hit() {
+        animator.Play("hit");
+    }
+
+    public void Activate() {
+        animator.Play("activate");
+    }
+
+    public void Atk01() {
+        animator.speed = 1.0f;
+        animator.Play("attack01");
+    }
+
+    public void Atk02() {
+        animator.speed = 1.0f;
+        animator.Play("attack02");
+    }
+
+    public void die() {
+        animator.Play("die");
+    }
+}
---
+++ Assets/UI/Assets/Icons/Attributes (Copyright)/Copyright Info.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyrightInfo : MonoBehaviour
+{
+    // Icons made by<a href="https://www.flaticon.com/authors/freepik" title="Freepik"> Freepik</a> from<a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+    // drill.png
+
+    // Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+    // crane.png
+
+    // Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+    // sketch.png
+
+    // Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+    // empty.png
+
+}
