b7889d8d5f165152e19ee3f6583f3e3114b873f8
Justin Lycklama <lycklama.justin@gmail.com>
2020-04-20 12:42:47 -0400

    Overview Panel, and other front facing ui overhaul

---
+++ Assets/UI/Modular Panels/OverviewPanel.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OverviewPanel : MonoBehaviour, GameButtonDelegate
+{
+    private const string SLIDE_OUT = "OverviewSlideOut";
+    private const string SLIDE_IN = "OverviewSlideIn";
+
+
+    [SerializeField]
+    private GameButton transitionButton;
+    [SerializeField]
+    private Image transitionButtonIcon;
+    [SerializeField]
+    private Animation anim;
+
+    [SerializeField]
+    private Sprite openIcon;
+    [SerializeField]
+    private Sprite closeIcon;
+
+    private bool minimized = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transitionButton.buttonDelegate = this;
+    }
+
+    /*
+     * GameButtonDelegate
+     * */
+
+    public void ButtonDidClick(GameButton button) {
+        if (minimized) {
+            anim.Play(SLIDE_OUT);
+            transitionButtonIcon.sprite = closeIcon;
+        } else {
+            anim.Play(SLIDE_IN);
+            transitionButtonIcon.sprite = openIcon;
+        }
+
+        minimized = !minimized;
+    }
+}
